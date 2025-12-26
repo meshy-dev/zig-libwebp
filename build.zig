edc0ccd35203441a6e1aeaffca4686c82e0d7470
Cheng Cao <bobcaocheng@gmail.com>
2025-12-25 19:02:40 -0800

    Fix build on nightly zig

--- build.zig
+++ build.zig
@@ -6,21 +6,6 @@ pub fn build(b: *std.Build) !void {
 
     const upstream = b.dependency("libwebp", .{});
 
-    // Core libwebp static library
-    const webp = b.addLibrary(.{
-        .name = "webp",
-        .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
-    });
-
-    webp.addIncludePath(upstream.path(""));
-    webp.addIncludePath(upstream.path("src"));
-    webp.addIncludePath(b.path(""));
-
     var common_flags: std.ArrayList([]const u8) = .empty;
     defer common_flags.deinit(b.allocator);
     try common_flags.appendSlice(b.allocator, &.{
@@ -29,93 +14,110 @@ pub fn build(b: *std.Build) !void {
         "-DHAVE_MALLOC_H",
     });
 
-    webp.addCSourceFiles(.{ .root = upstream.path(""), .files = webp_srcs, .flags = common_flags.items });
+    // Core libwebp static library
+    const webp_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    webp_mod.addIncludePath(upstream.path(""));
+    webp_mod.addIncludePath(upstream.path("src"));
+    webp_mod.addIncludePath(b.path(""));
+
+    webp_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = webp_srcs, .flags = common_flags.items });
+    const webp = b.addLibrary(.{
+        .name = "webp",
+        .linkage = .static,
+        .root_module = webp_mod,
+    });
 
     // webpdemux static library
-    const webpdemux = b.addLibrary(.{
-        .name = "webpdemux",
-        .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+    const webpdemux_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
-    webpdemux.addIncludePath(upstream.path(""));
-    webpdemux.addIncludePath(upstream.path("src"));
-    webpdemux.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+    webpdemux_mod.addIncludePath(upstream.path(""));
+    webpdemux_mod.addIncludePath(upstream.path("src"));
+    webpdemux_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "src/demux/anim_decode.c",
         "src/demux/demux.c",
     }, .flags = common_flags.items });
+    const webpdemux = b.addLibrary(.{
+        .name = "webpdemux",
+        .linkage = .static,
+        .root_module = webpdemux_mod,
+    });
 
     // webpmux static library
-    const webpmux = b.addLibrary(.{
-        .name = "webpmux",
-        .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+    const webpmux_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
-    webpmux.addIncludePath(upstream.path(""));
-    webpmux.addIncludePath(upstream.path("src"));
-    webpmux.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+    webpmux_mod.addIncludePath(upstream.path(""));
+    webpmux_mod.addIncludePath(upstream.path("src"));
+    webpmux_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "src/mux/anim_encode.c",
         "src/mux/muxedit.c",
         "src/mux/muxinternal.c",
         "src/mux/muxread.c",
     }, .flags = common_flags.items });
+    const webpmux = b.addLibrary(.{
+        .name = "webpmux",
+        .linkage = .static,
+        .root_module = webpmux_mod,
+    });
 
     // Support libraries for examples
+    const example_util_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    example_util_mod.addIncludePath(upstream.path(""));
+    example_util_mod.addIncludePath(upstream.path("src"));
+    example_util_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+        "examples/example_util.c",
+    }, .flags = common_flags.items });
+    example_util_mod.linkLibrary(webp);
     const example_util = b.addLibrary(.{
         .name = "example_util",
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+        .root_module = example_util_mod,
     });
-    example_util.addIncludePath(upstream.path(""));
-    example_util.addIncludePath(upstream.path("src"));
-    example_util.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
-        "examples/example_util.c",
-    }, .flags = common_flags.items });
-    example_util.linkLibrary(webp);
 
-    const imageio_util = b.addLibrary(.{
-        .name = "imageio_util",
-        .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
-    });
-    imageio_util.addIncludePath(upstream.path(""));
-    imageio_util.addIncludePath(upstream.path("src"));
     var imageio_flags: std.ArrayList([]const u8) = .empty;
     defer imageio_flags.deinit(b.allocator);
     try imageio_flags.appendSlice(b.allocator, common_flags.items);
     try imageio_flags.appendSlice(b.allocator, &.{"-DWEBP_HAVE_PNG"});
-    imageio_util.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+
+    const imageio_util_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    imageio_util_mod.addIncludePath(upstream.path(""));
+    imageio_util_mod.addIncludePath(upstream.path("src"));
+    imageio_util_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "imageio/imageio_util.c",
     }, .flags = imageio_flags.items });
-    imageio_util.linkLibrary(webp);
-
-    const imagedec = b.addLibrary(.{
-        .name = "imagedec",
+    imageio_util_mod.linkLibrary(webp);
+    const imageio_util = b.addLibrary(.{
+        .name = "imageio_util",
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+        .root_module = imageio_util_mod,
     });
-    imagedec.addIncludePath(upstream.path(""));
-    imagedec.addIncludePath(upstream.path("src"));
-    imagedec.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+
+    const imagedec_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    imagedec_mod.addIncludePath(upstream.path(""));
+    imagedec_mod.addIncludePath(upstream.path("src"));
+    imagedec_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "imageio/image_dec.c",
         "imageio/jpegdec.c",
         "imageio/metadata.c",
@@ -124,124 +126,135 @@ pub fn build(b: *std.Build) !void {
         "imageio/tiffdec.c",
         "imageio/webpdec.c",
     }, .flags = imageio_flags.items });
-    imagedec.linkLibrary(webpdemux);
-    imagedec.linkLibrary(webp);
+    imagedec_mod.linkLibrary(webpdemux);
+    imagedec_mod.linkLibrary(webp);
+    const imagedec = b.addLibrary(.{
+        .name = "imagedec",
+        .linkage = .static,
+        .root_module = imagedec_mod,
+    });
 
+    const imageenc_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    imageenc_mod.addIncludePath(upstream.path(""));
+    imageenc_mod.addIncludePath(upstream.path("src"));
+    imageenc_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+        "imageio/image_enc.c",
+    }, .flags = imageio_flags.items });
+    imageenc_mod.linkLibrary(webp);
+    imageenc_mod.linkLibrary(imageio_util);
     const imageenc = b.addLibrary(.{
         .name = "imageenc",
         .linkage = .static,
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+        .root_module = imageenc_mod,
     });
-    imageenc.addIncludePath(upstream.path(""));
-    imageenc.addIncludePath(upstream.path("src"));
-    imageenc.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
-        "imageio/image_enc.c",
-    }, .flags = imageio_flags.items });
-    imageenc.linkLibrary(webp);
-    imageenc.linkLibrary(imageio_util);
 
     // Executables
-    const cwebp = b.addExecutable(.{
-        .name = "cwebp",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+    const cwebp_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
-    cwebp.root_module.addIncludePath(upstream.path(""));
-    cwebp.root_module.addIncludePath(upstream.path("src"));
-    cwebp.root_module.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+    cwebp_mod.addIncludePath(upstream.path(""));
+    cwebp_mod.addIncludePath(upstream.path("src"));
+    cwebp_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "examples/cwebp.c",
     }, .flags = imageio_flags.items });
-    cwebp.root_module.linkLibrary(example_util);
-    cwebp.root_module.linkLibrary(imagedec);
-    cwebp.root_module.linkLibrary(imageio_util);
-    cwebp.root_module.linkLibrary(webpdemux);
-    cwebp.root_module.linkLibrary(webp);
+    cwebp_mod.linkLibrary(example_util);
+    cwebp_mod.linkLibrary(imagedec);
+    cwebp_mod.linkLibrary(imageio_util);
+    cwebp_mod.linkLibrary(webpdemux);
+    cwebp_mod.linkLibrary(webp);
+    const cwebp = b.addExecutable(.{
+        .name = "cwebp",
+        .root_module = cwebp_mod,
+    });
     b.installArtifact(cwebp);
 
-    const dwebp = b.addExecutable(.{
-        .name = "dwebp",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+    const dwebp_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
-    dwebp.root_module.addIncludePath(upstream.path(""));
-    dwebp.root_module.addIncludePath(upstream.path("src"));
-    dwebp.root_module.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+    dwebp_mod.addIncludePath(upstream.path(""));
+    dwebp_mod.addIncludePath(upstream.path("src"));
+    dwebp_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "examples/dwebp.c",
     }, .flags = imageio_flags.items });
-    dwebp.root_module.linkLibrary(example_util);
-    dwebp.root_module.linkLibrary(imagedec);
-    dwebp.root_module.linkLibrary(imageenc);
-    dwebp.root_module.linkLibrary(imageio_util);
-    dwebp.root_module.linkLibrary(webpdemux);
-    dwebp.root_module.linkLibrary(webp);
+    dwebp_mod.linkLibrary(example_util);
+    dwebp_mod.linkLibrary(imagedec);
+    dwebp_mod.linkLibrary(imageenc);
+    dwebp_mod.linkLibrary(imageio_util);
+    dwebp_mod.linkLibrary(webpdemux);
+    dwebp_mod.linkLibrary(webp);
+    const dwebp = b.addExecutable(.{
+        .name = "dwebp",
+        .root_module = dwebp_mod,
+    });
     b.installArtifact(dwebp);
 
-    const webpmux_example = b.addExecutable(.{
-        .name = "webpmux_example",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+    const webpmux_example_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
-    webpmux_example.root_module.addIncludePath(upstream.path(""));
-    webpmux_example.root_module.addIncludePath(upstream.path("src"));
-    webpmux_example.root_module.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+    webpmux_example_mod.addIncludePath(upstream.path(""));
+    webpmux_example_mod.addIncludePath(upstream.path("src"));
+    webpmux_example_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "examples/webpmux.c",
     }, .flags = imageio_flags.items });
-    webpmux_example.root_module.linkLibrary(example_util);
-    webpmux_example.root_module.linkLibrary(imageio_util);
-    webpmux_example.root_module.linkLibrary(webpmux);
-    webpmux_example.root_module.linkLibrary(webp);
+    webpmux_example_mod.linkLibrary(example_util);
+    webpmux_example_mod.linkLibrary(imageio_util);
+    webpmux_example_mod.linkLibrary(webpmux);
+    webpmux_example_mod.linkLibrary(webp);
+    const webpmux_example = b.addExecutable(.{
+        .name = "webpmux_example",
+        .root_module = webpmux_example_mod,
+    });
     b.installArtifact(webpmux_example);
 
-    const img2webp = b.addExecutable(.{
-        .name = "img2webp",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+    const img2webp_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
-    img2webp.root_module.addIncludePath(upstream.path(""));
-    img2webp.root_module.addIncludePath(upstream.path("src"));
-    img2webp.root_module.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+    img2webp_mod.addIncludePath(upstream.path(""));
+    img2webp_mod.addIncludePath(upstream.path("src"));
+    img2webp_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "examples/img2webp.c",
     }, .flags = imageio_flags.items });
-    img2webp.root_module.linkLibrary(example_util);
-    img2webp.root_module.linkLibrary(imagedec);
-    img2webp.root_module.linkLibrary(imageio_util);
-    img2webp.root_module.linkLibrary(webpmux);
-    img2webp.root_module.linkLibrary(webpdemux);
-    img2webp.root_module.linkLibrary(webp);
+    img2webp_mod.linkLibrary(example_util);
+    img2webp_mod.linkLibrary(imagedec);
+    img2webp_mod.linkLibrary(imageio_util);
+    img2webp_mod.linkLibrary(webpmux);
+    img2webp_mod.linkLibrary(webpdemux);
+    img2webp_mod.linkLibrary(webp);
+    const img2webp = b.addExecutable(.{
+        .name = "img2webp",
+        .root_module = img2webp_mod,
+    });
     b.installArtifact(img2webp);
 
-    const webpinfo = b.addExecutable(.{
-        .name = "webpinfo",
-        .root_module = b.createModule(.{
-            .target = target,
-            .optimize = optimize,
-            .link_libc = true,
-        }),
+    const webpinfo_mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
     });
-    webpinfo.root_module.addIncludePath(upstream.path(""));
-    webpinfo.root_module.addIncludePath(upstream.path("src"));
-    webpinfo.root_module.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
+    webpinfo_mod.addIncludePath(upstream.path(""));
+    webpinfo_mod.addIncludePath(upstream.path("src"));
+    webpinfo_mod.addCSourceFiles(.{ .root = upstream.path(""), .files = &.{
         "examples/webpinfo.c",
     }, .flags = imageio_flags.items });
-    webpinfo.root_module.linkLibrary(example_util);
-    webpinfo.root_module.linkLibrary(imageio_util);
-    webpinfo.root_module.linkLibrary(webp);
+    webpinfo_mod.linkLibrary(example_util);
+    webpinfo_mod.linkLibrary(imageio_util);
+    webpinfo_mod.linkLibrary(webp);
+    const webpinfo = b.addExecutable(.{
+        .name = "webpinfo",
+        .root_module = webpinfo_mod,
+    });
     b.installArtifact(webpinfo);
 
     // Link against zlib and libpng for imageio where applicable.
@@ -249,10 +262,10 @@ pub fn build(b: *std.Build) !void {
     const png_dep = b.dependency("libpng", .{ .target = target, .optimize = optimize });
 
     // imageio requires system libs when available: png, jpeg, tiff. We provide png via dependency.
-    for ([_]*std.Build.Step.Compile{ imagedec, imageenc, imageio_util, cwebp, dwebp, webpmux_example, img2webp, webpinfo }) |comp| {
-        comp.linkLibrary(z_dep.artifact("z"));
-        comp.linkLibrary(png_dep.artifact("png"));
-        comp.addIncludePath(png_dep.path(""));
+    for ([_]*std.Build.Module{ imagedec_mod, imageenc_mod, imageio_util_mod, cwebp_mod, dwebp_mod, webpmux_example_mod, img2webp_mod, webpinfo_mod }) |mod| {
+        mod.linkLibrary(z_dep.artifact("z"));
+        mod.linkLibrary(png_dep.artifact("png"));
+        mod.addIncludePath(png_dep.path(""));
     }
 
     // Install core libraries and headers.
